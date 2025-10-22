5e3d61a0548e07267998d9005718db3a8b2d583c
Neill Robson <neill@neillrobson.com>
2025-10-22 10:21:04 -0400

    Chapter 4

---
+++ src/ch4.hs
@@ -0,0 +1,14 @@
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome x = x == reverse x
+
+myAbs :: Integer -> Integer
+myAbs x = if x > 0 then x else -x
+
+f :: (a, b) -> (c, d) -> ((b, d), (a, c))
+f (a, b) (c, d) = ((b, d), (a, c))
+
+x = (+)
+
+myF xs = w `x` 1
+  where
+    w = length xs
