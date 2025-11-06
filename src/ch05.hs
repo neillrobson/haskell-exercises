005ffa89c8b2c7d96a170f8810260a98bc913e0f
Neill Robson <neill@neillrobson.com>
2025-11-06 15:39:06 -0500

    Ch5 continued

--- src/ch05.hs
+++ src/ch05.hs
@@ -6,3 +6,20 @@ functionC x y = x > y
 
 functionS :: (a, b) -> b
 functionS (x, y) = y
+
+-----------------------------------------------
+
+i :: a -> a
+i x = x
+
+c :: a -> b -> a
+c x y = x
+
+c' :: a -> b -> b
+c' x y = y
+
+r :: [a] -> [a]
+r (_ : xs) = xs
+
+co :: (b -> c) -> (a -> b) -> a -> c
+co f g x = f $ g x
