0c7cb8b69be33026ecdb11fe906eebcde12fd334
Neill Robson <neill@neillrobson.com>
2025-11-14 14:44:45 -0500

    number to word implementation

--- src/wordnumber.hs
+++ src/wordnumber.hs
@@ -1,12 +1,27 @@
 module WordNumber where
 
-import Data.List (intersperse)
+import Data.List (intercalate)
 
 digitToWord :: Int -> String
-digitToWord n = undefined
+digitToWord n = case n of
+  0 -> "zero"
+  1 -> "one"
+  2 -> "two"
+  3 -> "three"
+  4 -> "four"
+  5 -> "five"
+  6 -> "six"
+  7 -> "seven"
+  8 -> "eight"
+  9 -> "nine"
+  _ -> error "argument is not a single digit"
 
 digits :: Int -> [Int]
-digits n = undefined
+digits n = go n []
+  where
+    go i acc
+      | i == 0 = acc
+      | otherwise = go (div i 10) (mod i 10 : acc)
 
 wordNumber :: Int -> String
-wordNumber n = undefined
+wordNumber = intercalate "-" . map digitToWord . digits
