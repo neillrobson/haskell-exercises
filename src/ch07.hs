db5c7b89caa91fec5bd39b0fff8770feb3acb0d9
Neill Robson <neill@neillrobson.com>
2025-11-14 13:31:57 -0500

    My first polyvariadic Haskell function

--- src/ch07.hs
+++ src/ch07.hs
@@ -1,12 +1,15 @@
+addOneIfOdd :: (Integral a) => a -> a
 addOneIfOdd n = if odd n then f n else n
   where
-    f n = n + 1
+    f n' = n' + 1
 
+addFive :: (Num a, Ord a) => a -> a -> a
 addFive x y = min x y + 5
 
-f :: (a, b, c) -> (d, e, f) -> ((a, d), (c, f))
-f (a, b, c) (d, e, f) = ((a, d), (c, f))
+f1 :: (a, b, c) -> (d, e, f) -> ((a, d), (c, f))
+f1 (a, _, c) (d, _, f) = ((a, d), (c, f))
 
+nums :: (Ord a1, Num a1, Num a2) => a1 -> a2
 nums x =
   case compare x 0 of
     LT -> -1
@@ -21,3 +24,20 @@ oneIsOne = dodgy 1
 
 oneIsTwo :: (Num a) => a -> a
 oneIsTwo = flip dodgy 2
+
+-- My own exercise:
+-- Create the type declaration and implementation of a function that Shows an arbitrary number of arguments.
+
+class ShowArgsType t where
+  catArg :: (Show a) => a -> String -> t
+
+instance ShowArgsType String where
+  catArg :: (Show a) => a -> String -> String
+  catArg next acc = show next ++ acc
+
+instance (Show a, ShowArgsType r) => ShowArgsType (a -> r) where
+  catArg :: (Show i) => i -> String -> a -> r
+  catArg next acc a = catArg next $ acc ++ show a
+
+showArgs :: (Show a, ShowArgsType r) => a -> r
+showArgs a = catArg a ""
