b215fb3357fce9792890de0f5e049de5657143c7
Neill Robson <neill@neillrobson.com>
2025-11-20 15:49:07 -0500

    Finish ch9 exercises

--- src/09.hs
+++ src/09.hs
@@ -16,3 +16,38 @@ myElem e (x : xs) = e == x || myElem e xs
 
 myElemAny :: (Eq a) => a -> [a] -> Bool
 myElemAny e = any (== e)
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x : xs) = myReverse xs ++ [x]
+
+squish :: [[a]] -> [a]
+squish [] = undefined
+squish [l] = l
+squish (l : ls) = l ++ squish ls
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap f = squish . map f
+
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap id
+
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy _ [] = undefined
+myMaximumBy _ [x] = x
+myMaximumBy cmp (x : xs) =
+  let y = myMaximumBy cmp xs
+   in if x `cmp` y == GT then x else y
+
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy _ [] = undefined
+myMinimumBy _ [x] = x
+myMinimumBy cmp (x : xs) =
+  let y = myMinimumBy cmp xs
+   in if x `cmp` y == LT then x else y
+
+myMaximum :: (Ord a) => [a] -> a
+myMaximum = myMaximumBy compare
+
+myMinimum :: (Ord a) => [a] -> a
+myMinimum = myMinimumBy compare
