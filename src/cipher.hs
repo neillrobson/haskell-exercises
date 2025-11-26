436d2613acdba641e0ac36e7d6b04e25b3ae4abc
Neill Robson <neill@neillrobson.com>
2025-11-26 12:44:05 -0500

    Add Vigenere cipher

--- src/cipher.hs
+++ src/cipher.hs
@@ -5,8 +5,20 @@ import Data.Char (chr, ord)
 caesarChrBy :: Int -> Char -> Char
 caesarChrBy n = chr . (+ ord 'a') . flip mod 26 . (+ n) . subtract (ord 'a') . ord
 
+caesarChrWith :: Char -> Char -> Char
+caesarChrWith = caesarChrBy . subtract (ord 'a') . ord
+
 caesar :: String -> String
 caesar = map $ caesarChrBy 5
 
 unCaesar :: String -> String
 unCaesar = map $ caesarChrBy (-5)
+
+type Keyword = String
+
+type Message = String
+
+type Cipher = String
+
+vigenere :: Keyword -> Message -> Cipher
+vigenere k = zipWith caesarChrWith (cycle k)
