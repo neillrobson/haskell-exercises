1e5055f840e787a09581267801ad1162a8a5eda4
Neill Robson <neill@neillrobson.com>
2025-12-01 14:15:06 -0500

    Finish string processing

--- src/12.hs
+++ src/12.hs
@@ -14,7 +14,22 @@ countTheBeforeVowel = go . words
     go [_] = 0
     go ws@(t : v : _) =
       let n = go $ tail ws
-       in if isNothing (notThe t) && head v `elem` "aeiou" then 1 + n else n
+       in if isNothing (notThe t) && head v `elem` vowels then 1 + n else n
 
 countVowels :: String -> Integer
-countVowels = toInteger . length . filter (`elem` "aeiou")
+countVowels = toInteger . length . filter (`elem` vowels)
+
+newtype Word' = Word' String deriving (Eq, Show)
+
+vowels :: String
+vowels = "aeiou"
+
+letterCounts :: String -> (Integer, Integer)
+letterCounts = foldr go (0, 0)
+  where
+    go i (v, c) = if i `elem` vowels then (v + 1, c) else (v, c + 1)
+
+mkWord' :: String -> Maybe Word'
+mkWord' s =
+  let (v, c) = letterCounts s
+   in if v > c then Nothing else Just $ Word' s
