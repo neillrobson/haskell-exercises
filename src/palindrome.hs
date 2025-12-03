50030ef95f41f9a68e727c0eedcb6458b90ba505
Neill Robson <neill@neillrobson.com>
2025-12-03 16:19:06 -0500

    Normalize palindrome checker

--- src/palindrome.hs
+++ src/palindrome.hs
@@ -1,10 +1,15 @@
 import Control.Monad
+import Data.Char (toLower)
 import Distribution.Compat.Prelude (exitSuccess)
+
+justalpha :: String -> String
+justalpha = filter (`elem` "abcdefghijklmnopqrstuvwxyz") . map toLower
 
 palindrome :: IO ()
 palindrome = forever $ do
   line1 <- getLine
-  if line1 == reverse line1
+  let normalized = justalpha line1
+  if normalized == reverse normalized
     then putStrLn "It's a palindrome!"
     else do
       putStrLn "Nope!"
