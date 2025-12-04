3180e702204ee42fba62d06608bf4a9b9c4d74ce
Neill Robson <neill@neillrobson.com>
2025-12-04 09:52:51 -0500

    Test division

--- src/Addition.hs
+++ src/Addition.hs
@@ -1,4 +1,18 @@
 module Addition where
 
-sayHello :: IO ()
-sayHello = putStrLn "hello!"
+import Test.Hspec
+
+dividedBy :: (Integral a) => a -> a -> (a, a)
+dividedBy num denom = go num denom 0
+  where
+    go n d count
+      | n >= d = go (n - d) d (count + 1)
+      | otherwise = (count, n)
+
+main :: IO ()
+main = hspec $ do
+  describe "Addition" $ do
+    it "15 divided by 3 is 5" $ do
+      dividedBy 15 3 `shouldBe` (5, 0 :: Integer)
+    it "22 divided by 5 is 4 remainder 2" $ do
+      dividedBy 22 5 `shouldBe` (4, 2 :: Integer)
