3f71a14b16d5d1eadb17b7f3b499824e2f9d3458
Neill Robson <neill@neillrobson.com>
2025-12-05 16:23:01 -0500

    Begin ch15 exercises

---
+++ src/15.hs
@@ -0,0 +1,36 @@
+import Test.QuickCheck (Arbitrary (arbitrary), quickCheck)
+
+data Trivial = Trivial deriving (Eq, Show)
+
+instance Semigroup Trivial where
+  _ <> _ = Trivial
+
+instance Arbitrary Trivial where
+  arbitrary = return Trivial
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = a <> (b <> c) == (a <> b) <> c
+
+type TrivAssoc = Trivial -> Trivial -> Trivial -> Bool
+
+-------------------------------------------------------------------
+
+data Two a b = Two a b deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
+  (Two a b) <> (Two a' b') = Two (a <> a') (b <> b')
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    return $ Two a b
+
+type TwoAssoc = Two String String -> Two String String -> Two String String -> Bool
+
+-------------------------------------------------------------------
+
+main :: IO ()
+main = do
+  quickCheck (semigroupAssoc :: TrivAssoc)
+  quickCheck (semigroupAssoc :: TwoAssoc)
