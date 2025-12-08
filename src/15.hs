9f69f105198158390209fcd01a864a0eaa20045d
Neill Robson <neill@neillrobson.com>
2025-12-08 13:52:26 -0500

    Add two more semigroup impls

--- src/15.hs
+++ src/15.hs
@@ -1,4 +1,8 @@
-import Test.QuickCheck (Arbitrary (arbitrary), quickCheck)
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use <$>" #-}
+import Data.Monoid (Sum)
+import Test.QuickCheck (Arbitrary (arbitrary), CoArbitrary, oneof, quickCheck)
 
 data Trivial = Trivial deriving (Eq, Show)
 
@@ -30,7 +34,44 @@ type TwoAssoc = Two String String -> Two String String -> Two String String -> B
 
 -------------------------------------------------------------------
 
+data Or a b = Fst a | Snd b deriving (Eq, Show)
+
+instance Semigroup (Or a b) where
+  sb@(Snd _) <> _ = sb
+  _ <> sb@(Snd _) = sb
+  a <> _ = a
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    oneof [return $ Fst a, return $ Snd b]
+
+type OrAssoc = Or Int String -> Or Int String -> Or Int String -> Bool
+
+-------------------------------------------------------------------
+
+newtype Combine a b = Combine {unCombine :: a -> b}
+
+instance Show (Combine a b) where
+  show _ = "Combine"
+
+instance (Semigroup b) => Semigroup (Combine a b) where
+  (Combine f) <> (Combine g) = Combine $ \a -> f a <> g a
+
+instance (CoArbitrary a, Arbitrary b) => Arbitrary (Combine a b) where
+  arbitrary = Combine <$> arbitrary
+
+combSemigroupAssoc :: (Eq b, Semigroup b) => Combine a b -> Combine a b -> Combine a b -> a -> Bool
+combSemigroupAssoc f g h a = unCombine (f <> (g <> h)) a == unCombine ((f <> g) <> h) a
+
+type CombAssoc = Combine Int (Sum Int) -> Combine Int (Sum Int) -> Combine Int (Sum Int) -> Int -> Bool
+
+-------------------------------------------------------------------
+
 main :: IO ()
 main = do
   quickCheck (semigroupAssoc :: TrivAssoc)
   quickCheck (semigroupAssoc :: TwoAssoc)
+  quickCheck (semigroupAssoc :: OrAssoc)
+  quickCheck (combSemigroupAssoc :: CombAssoc)
