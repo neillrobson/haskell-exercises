5385c41a842737220459be81221bb639480d0e6a
Neill Robson <neill@neillrobson.com>
2026-01-05 12:27:19 -0500

    Constant applicative instance

--- src/typecheckapp.hs
+++ src/typecheckapp.hs
@@ -49,3 +49,14 @@ instance Functor Identity where
 instance Applicative Identity where
   pure = Identity
   (Identity f) <*> (Identity a) = Identity $ f a
+
+--------------------------------------------------------------------------------
+
+newtype Constant a b = Constant {getConstant :: a} deriving (Eq, Ord, Show)
+
+instance Functor (Constant a) where
+  fmap _ (Constant c) = Constant c
+
+instance (Monoid a) => Applicative (Constant a) where
+  pure _ = Constant mempty
+  (Constant c) <*> (Constant c') = Constant $ c <> c'
