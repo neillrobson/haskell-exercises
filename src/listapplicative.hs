af2c98dba3e0ac208ecd7814a10cb06f7742b223
Neill Robson <neill@neillrobson.com>
2026-01-08 10:47:15 -0500

    Naive implementation of ziplist applicative

--- src/listapplicative.hs
+++ src/listapplicative.hs
@@ -41,5 +41,36 @@ instance (Arbitrary a) => Arbitrary (List a) where
 instance (Eq a) => EqProp (List a) where
   (=-=) = eq
 
+--------------------------------------------------------------------------------
+
+newtype ZipList' a = ZipList' [a] deriving (Eq, Show)
+
+instance Functor ZipList' where
+  fmap f (ZipList' xs) = ZipList' $ fmap f xs
+
+instance Applicative ZipList' where
+  pure a = ZipList' $ repeat a
+  (ZipList' []) <*> _ = ZipList' []
+  _ <*> (ZipList' []) = ZipList' []
+  (ZipList' (x : xs)) <*> (ZipList' (y : ys)) = ZipList' $ z : zs
+    where
+      z = x y
+      (ZipList' zs) = (ZipList' xs) <*> (ZipList' ys)
+
+--------------------------------------------------------------------------------
+
+instance (Arbitrary a) => Arbitrary (ZipList' a) where
+  arbitrary = ZipList' <$> arbitrary
+
+instance (Eq a) => EqProp (ZipList' a) where
+  xs =-= ys = xs' `eq` ys'
+    where
+      xs' = let (ZipList' l) = xs in take 3000 l
+      ys' = let (ZipList' l) = ys in take 3000 l
+
+--------------------------------------------------------------------------------
+
 main :: IO ()
-main = quickBatch $ applicative $ Cons ('a', 'b', 'c') Nil
+main = do
+  quickBatch $ applicative $ Cons ('a', 'b', 'c') Nil
+  quickBatch $ applicative $ ZipList' [('a', 'b', 'c')]
