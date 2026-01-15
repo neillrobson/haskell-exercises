a226e75e1247891711b6bfd34b004b10dae71c05
Neill Robson <neill@neillrobson.com>
2026-01-15 13:28:37 -0500

    Finish ch20 exercises

--- src/20.hs
+++ src/20.hs
@@ -1,3 +1,4 @@
+import Data.Function (on)
 import Data.Monoid (Any (Any, getAny), Product (Product, getProduct), Sum (Sum, getSum))
 
 sum :: (Foldable t, Num a) => t a -> a
@@ -35,3 +36,37 @@ fold = foldMap id
 
 myFoldMap :: (Foldable t, Monoid m) => (a -> m) -> t a -> m
 myFoldMap g = foldr ((<>) . g) mempty
+
+--------------------------------------------------------------------------------
+
+data Constant a b = Constant b
+
+instance Foldable (Constant a) where
+  foldMap g (Constant b) = g b
+
+data Two a b = Two a b
+
+instance Foldable (Two a) where
+  foldMap g (Two _ b) = g b
+
+data Three a b c = Three a b c
+
+instance Foldable (Three a b) where
+  foldMap g (Three _ _ c) = g c
+
+data Three' a b = Three' a b b
+
+instance Foldable (Three' a) where
+  foldMap g (Three' _ x y) = on (<>) g x y
+
+data Four' a b = Four' a b b b
+
+instance Foldable (Four' a) where
+  foldMap g (Four' _ x y z) = (g x) <> (g y) <> (g z)
+
+--------------------------------------------------------------------------------
+
+filterF :: (Applicative f, Foldable t, Monoid (f a)) => (a -> Bool) -> t a -> f a
+filterF g = foldMap go
+  where
+    go x = if g x then pure x else mempty
