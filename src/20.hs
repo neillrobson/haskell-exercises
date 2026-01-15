61914128c3ab25f4f3478071c1dcf4904a7e1bba
Neill Robson <neill@neillrobson.com>
2026-01-15 13:02:33 -0500

    Foldable utilities reimplemented

--- src/20.hs
+++ src/20.hs
@@ -1,7 +1,37 @@
-import Data.Monoid (Product (Product, getProduct), Sum (Sum, getSum))
+import Data.Monoid (Any (Any, getAny), Product (Product, getProduct), Sum (Sum, getSum))
 
 sum :: (Foldable t, Num a) => t a -> a
 sum = getSum . (foldMap Sum)
 
 product :: (Foldable t, Num a) => t a -> a
 product = getProduct . (foldMap Product)
+
+elem :: (Foldable t, Eq a) => a -> t a -> Bool
+elem x = getAny . (foldMap $ Any . (== x))
+
+minimum :: (Foldable t, Ord a) => t a -> Maybe a
+minimum = foldr go Nothing
+  where
+    go x Nothing = Just x
+    go x (Just y) = Just $ min x y
+
+maximum :: (Foldable t, Ord a) => t a -> Maybe a
+maximum = foldr go Nothing
+  where
+    go x Nothing = Just x
+    go x (Just y) = Just $ max x y
+
+null :: (Foldable t) => t a -> Bool
+null = foldr (const . const True) False
+
+length :: (Foldable t) => t a -> Int
+length = foldr (\_ -> (+ 1)) 0
+
+toList :: (Foldable t) => t a -> [a]
+toList = foldr (:) []
+
+fold :: (Foldable t, Monoid m) => t m -> m
+fold = foldMap id
+
+myFoldMap :: (Foldable t, Monoid m) => (a -> m) -> t a -> m
+myFoldMap g = foldr ((<>) . g) mempty
