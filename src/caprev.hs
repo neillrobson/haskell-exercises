844814dc2a83038a78d3fd4d0e7f805494df4a9d
Neill Robson <neill@neillrobson.com>
2026-01-20 09:21:45 -0500

    Begin ch22 exercises

---
+++ src/caprev.hs
@@ -0,0 +1,38 @@
+import Data.Char
+
+cap :: [Char] -> [Char]
+cap = map toUpper
+
+rev :: [Char] -> [Char]
+rev = reverse
+
+composed :: String -> String
+composed = cap . rev
+
+fmapped :: String -> String
+fmapped = fmap cap rev
+
+tupled :: String -> (String, String)
+tupled = do
+  capped <- cap
+  revved <- rev
+  return (capped, revved)
+
+tupled' :: String -> (String, String)
+tupled' = (,) <$> cap <*> rev
+
+tupled'' :: String -> (String, String)
+tupled'' = cap >>= \c -> rev >>= \r -> return (c, r)
+
+--------------------------------------------------------------------------------
+
+myLiftA2 :: (Applicative f) => (a -> b -> c) -> f a -> f b -> f c
+myLiftA2 g fa fb = g <$> fa <*> fb
+
+newtype Reader r a = Reader {getReader :: r -> a}
+
+ask :: Reader a a
+ask = Reader id
+
+asks :: (r -> a) -> Reader r a
+asks f = Reader f
