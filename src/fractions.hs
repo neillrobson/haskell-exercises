55575847b8a2acdc7ece079bd0d88c6d5a739fb8
Neill Robson <neill@neillrobson.com>
2026-01-28 14:58:51 -0500

    Start fraction parsing

---
+++ src/fractions.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Text.Fractions where
+
+import Control.Applicative
+import Data.Ratio ((%))
+import Text.Trifecta
+
+badFraction :: String
+badFraction = "1/0"
+
+alsoBad :: String
+alsoBad = "10"
+
+shouldWork :: String
+shouldWork = "1/2"
+
+shouldAlsoWork :: String
+shouldAlsoWork = "2/1"
+
+parseFraction :: Parser Rational
+parseFraction = do
+  numerator <- decimal
+  _ <- char '/'
+  denominator <- decimal
+  return $ numerator % denominator
+
+virtuousFraction :: Parser Rational
+virtuousFraction = do
+  numerator <- decimal
+  _ <- char '/'
+  denominator <- decimal
+  case denominator of
+    0 -> fail "Denominator cannot be zero"
+    _ -> return $ numerator % denominator
+
+main :: IO ()
+main = do
+  let parseFraction' = parseString parseFraction mempty
+
+  print $ parseFraction' shouldWork
+  print $ parseFraction' shouldAlsoWork
+
+  print $ parseFraction' alsoBad
+  print $ parseFraction' badFraction
+
+testVirtuous :: IO ()
+testVirtuous = do
+  let parseFraction' = parseString virtuousFraction mempty
+
+  print $ parseFraction' alsoBad
+  print $ parseFraction' badFraction
+
+  print $ parseFraction' shouldWork
+  print $ parseFraction' shouldAlsoWork
