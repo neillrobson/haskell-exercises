b383b00e0e12c09e550af7ef2a90ece4a60b660f
Neill Robson <neill@neillrobson.com>
2026-03-02 13:56:15 -0500

    StateT Functor improvement

--- src/statet.hs
+++ src/statet.hs
@@ -6,7 +6,7 @@ newtype StateT s m a = StateT {runStateT :: s -> m (a, s)}
 
 instance (Functor m) => Functor (StateT s m) where
   fmap :: (Functor m) => (a -> b) -> StateT s m a -> StateT s m b
-  fmap f (StateT sma) = StateT $ (fmap . fmap) (first f) sma
+  fmap f (StateT sma) = StateT $ fmap (first f) . sma
 
 instance (Monad m) => Applicative (StateT s m) where
   pure :: (Monad m) => a -> StateT s m a
