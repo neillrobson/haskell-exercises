ff2035227ee1dd00d460e0806b458bac102bfc5b
Neill Robson <neill@neillrobson.com>
2026-03-03 13:35:48 -0500

    AAM ex2

--- src/all-about-monads/sheep.hs
+++ src/all-about-monads/sheep.hs
@@ -2,6 +2,8 @@
 
 module Sheep where
 
+import Control.Monad (mplus)
+
 data Sheep = Sheep {name :: String, mother :: Maybe Sheep, father :: Maybe Sheep}
 
 instance Show Sheep where
@@ -25,6 +27,18 @@ mothersPaternalGrandfather s = do
   gf <- father g
   father gf
 
+breedSheep :: Sheep
+breedSheep =
+  let adam = Sheep "Adam" Nothing Nothing
+      eve = Sheep "Eve" Nothing Nothing
+      uranus = Sheep "Uranus" Nothing Nothing
+      gaea = Sheep "Gaea" Nothing Nothing
+      kronos = Sheep "Kronos" (Just gaea) (Just uranus)
+      holly = Sheep "Holly" (Just eve) (Just adam)
+      roger = Sheep "Roger" (Just eve) (Just kronos)
+      molly = Sheep "Molly" (Just holly) (Just roger)
+   in Sheep "Dolly" (Just molly) Nothing
+
 --------------------------------------------------------------------------------
 
 mgf :: Sheep -> Maybe Sheep
@@ -35,3 +49,11 @@ fmgm s = father s >>= mother >>= mother
 
 mpgf :: Sheep -> Maybe Sheep
 mpgf s = mother s >>= father >>= father
+
+--------------------------------------------------------------------------------
+
+parent :: Sheep -> Maybe Sheep
+parent s = mother s `mplus` father s
+
+grandparent :: Sheep -> Maybe Sheep
+grandparent s = let p = parent s in (p >>= mother) `mplus` (p >>= father)
