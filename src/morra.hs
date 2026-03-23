ca9c003edf6bcd837470814f8dd514f6fd359b86
Neill Robson <neill@neillrobson.com>
2026-03-23 15:28:34 -0400

    Morra type specifications

--- src/morra.hs
+++ src/morra.hs
@@ -1,4 +1,28 @@
 module Morra where
 
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.State (StateT (runStateT))
+
+data Config = Config
+  { pScore :: Integer,
+    cScore :: Integer
+  }
+  deriving (Show)
+
+type Game = StateT Config IO
+
+app :: Game ()
+app = do
+  lift $ putStrLn "-- P is Player"
+  lift $ putStrLn "-- C is Computer"
+  lift $ putStrLn "-- Player is odds, Computer is evens."
+  lift $ putStr "P: "
+  p <- lift getLine
+  lift $ putStrLn $ "- P wrote " ++ p
+
 main :: IO ()
-main = undefined
+main = do
+  let config = Config 0 0
+  (unit, state) <- runStateT app config
+  putStrLn $ "unit: " ++ show unit
+  putStrLn $ "state: " ++ show state
