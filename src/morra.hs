f8e6d5e31eb5494cffeb34eede2fc87d393ef1c9
Neill Robson <neill@neillrobson.com>
2026-03-26 10:30:16 -0400

    Add updateHistory fn, untested

--- src/morra.hs
+++ src/morra.hs
@@ -7,15 +7,30 @@ import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Maybe
 import Control.Monad.Trans.State
+import qualified Data.Map as M
 import System.Random (randomRIO)
+
+type History = M.Map (Integer, Integer) Integer
 
 data GameState = GameState
   { pScore :: Integer,
-    cScore :: Integer
+    cScore :: Integer,
+    history :: History,
+    moves :: (Integer, Integer, Integer)
   }
   deriving (Show)
 
 type Game = MaybeT (StateT GameState IO)
+
+updateHistory :: Integer -> Game ()
+updateHistory i = do
+  s <- lift get
+  let (b, a, _) = moves s
+      hist = history s
+      ms' = (i, b, a)
+  lift . put $ s {moves = ms'}
+  key <- if a == 0 then mzero else return (a, b)
+  lift . put $ s {history = M.insert key i hist}
 
 doRound :: Game ()
 doRound = do
@@ -35,6 +50,7 @@ doRound = do
     else do
       liftIO $ putStrLn "- P wins"
       lift . modify $ \s@(pScore -> ps) -> s {pScore = succ ps}
+  updateHistory p
 
 start :: Game ()
 start = forever doRound
@@ -45,7 +61,7 @@ main = do
   putStrLn "-- C is Computer"
   putStrLn "-- Player is odds, Computer is evens."
   putStrLn "-- (any invalid input immediately ends the game.)"
-  let config = GameState 0 0
+  let config = GameState 0 0 M.empty (0, 0, 0)
   result <- execStateT (runMaybeT start) config
   let p = pScore result
       c = cScore result
