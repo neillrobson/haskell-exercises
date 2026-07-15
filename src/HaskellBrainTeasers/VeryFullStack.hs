9cfd05dca0a6404de02b367132e5d006d86b8789
Neill Robson <neill@neillrobson.com>
2026-07-15 15:33:01 -0400

    very full stack

---
+++ src/HaskellBrainTeasers/VeryFullStack.hs
@@ -0,0 +1,33 @@
+module HaskellBrainTeasers.VeryFullStack where
+
+data RoseTree a = RoseTree a [RoseTree a]
+
+breadthFirst :: RoseTree a -> [a]
+breadthFirst (RoseTree a trees) = a : go trees
+  where
+    go [] = []
+    go subtrees =
+      let (vals, subs) = foldr accumRoseTree ([], []) subtrees
+       in vals <> go subs
+    accumRoseTree (RoseTree v t) (vs, ts) = (v : vs, t <> ts)
+
+main :: IO ()
+main =
+  if "Maple" `elem` breadthFirst forest
+    then putStrLn "pancake time"
+    else putStrLn "no syrup"
+  where
+    forest =
+      RoseTree
+        "Aspen"
+        [ RoseTree
+            "Baobab"
+            [ RoseTree "Paw Paw" [],
+              RoseTree "Basswood" []
+            ],
+          RoseTree
+            "Cherry"
+            [ RoseTree "Maple" [],
+              error "boom"
+            ]
+        ]
